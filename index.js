66db0a89707dd0f49c07033cea874de334343fca
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 10:55:48 +1200

    Debugging

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require("debug")("app:startup");
+const dbDebugger = require("debug")("app:db");
 const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -23,8 +25,11 @@ app.use(express.static("public"));
 app.use(helmet());
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  console.log("Morgan enabled...");
+  startupDebugger("Morgan enabled...");
 }
+
+// Db work...
+dbDebugger("Connected to the database...");
 
 // Custom middlewares
 app.use(logger);
