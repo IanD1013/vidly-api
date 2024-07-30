58ce52cee0d0f6907a1fd4fa115f5add9ce289fc
IanD1013 <ian.tung1013@gmail.com>
2024-07-30 22:36:30 +1200

    Handle http POST request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
@@ -17,8 +19,21 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
+
   if (!course)
     return res.status(404).send("The course with the given ID was not found.");
+
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+
   res.send(course);
 });
 
