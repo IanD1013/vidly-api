e4abf895bd8a2e1ecdb6aebe7a7aec9ae30855ed
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 09:42:01 +1200

    Built-in middleware

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.use(logger);
 
 const courses = [
