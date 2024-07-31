cbd6f08530f21a99f5649789bb1f47d3755b3742
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 13:18:42 +1200

    Named functions to rescue

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,19 +1,18 @@
-const { get } = require("config");
-
 console.log("Before");
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repo, (commits) => {
-      // Callback Hell
-    });
-  });
-});
+getUser(1, getRepositories);
 console.log("After");
 
-// 3 patterns to deal with async code
-// 1. Callbacks
-// 2. Promises
-// 3. Async/await (syntactic sugar over promises)
+function getRepositories(user) {
+  getRepositories(user.gitHubUsername, getCommits);
+}
+
+function getCommits(repo) {
+  getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log(commits);
+}
 
 function getUser(id, callback) {
   setTimeout(() => {
