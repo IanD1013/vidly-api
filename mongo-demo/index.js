185d849a15498d1d2a158b0c2877bbd493b1a5d9
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 22:43:42 +1200

    Logical query operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,6 +31,10 @@ async function getCourses() {
   const courses = await Course.find({ author: "Mosh", isPublished: true })
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find({ price: { $in: [10, 15, 20] } })
+
+    // .find()
+    // .or([ { author: 'Mosh' }, { isPublished: true } ])
+    // .and([ ])
     .limit(10)
     .sort({ name: 1 }) // 1 for ascending, -1 for descending
     .select({ name: 1, tags: 1 }); // 1 for including, 0 for excluding
