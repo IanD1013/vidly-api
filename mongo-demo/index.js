95d630b722b8b7f80eee4b0215aa0cac09bd26dc
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 21:53:51 +1200

    Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,6 +1,17 @@
+const { date } = require("joi");
 const mongoose = require("mongoose");
 
+// 1. Connect to MongoDB
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// 2. Create a schema
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
