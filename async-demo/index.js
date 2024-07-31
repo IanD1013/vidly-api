67b431a8a4bec9368ae279091cfbfe249f75ed32
IanD1013 <ian.tung1013@gmail.com>
2024-07-31 19:00:40 +1200

    Replace callbacks with promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -14,16 +14,30 @@ function displayCommits(commits) {
   console.log(commits);
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({ id: id, gitHubUsername: "mosh" });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({ id: id, gitHubUsername: "mosh" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("Calling GitHub API...");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
 }
