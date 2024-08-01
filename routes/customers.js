da97c932c5584ec2155ea9f54170652d6f2835c3
Ian-Dzy <zdon070@aucklanduni.ac.nz>
2024-08-01 20:09:57 +1200

    Restructure the project

--- routes/customers.js
+++ routes/customers.js
@@ -1,29 +1,6 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const { Customer, validateCustomer } = require("../models/customer");
 const express = require("express");
 const router = express.Router();
-
-const Customer = mongoose.model(
-  "Customer",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-    isGold: {
-      type: Boolean,
-      default: false,
-    },
-    phone: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort("name");
@@ -67,7 +44,7 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  const customer = await Customer.findByIdAndRemove(req.params.id);
+  const customer = await Customer.findByIdAndDelete(req.params.id);
 
   if (!customer)
     return res
@@ -87,15 +64,5 @@ router.get("/:id", async (req, res) => {
 
   res.send(customer);
 });
-
-function validateCustomer(customer) {
-  const schema = Joi.object({
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean(),
-  });
-
-  return schema.validate(customer);
-}
 
 module.exports = router;
--- routes/genres.js
+++ routes/genres.js
@@ -1,13 +1,6 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const { Genre, validateGenre } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
-
-const genreSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 5, maxlength: 50 },
-});
-
-const Genre = mongoose.model("Genre", genreSchema);
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find().sort("name");
@@ -56,13 +49,5 @@ router.get("/:id", async (req, res) => {
 
   res.send(genre);
 });
-
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(genre);
-}
 
 module.exports = router;
