b3154a941e87a29e91ac5d1040045c7012e1f1d4
IanD1013 <ian.tung1013@gmail.com>
2024-08-03 21:48:40 +1200

    Jest mock functions

--- playground/demo-testing/tests/lib.test.js
+++ playground/demo-testing/tests/lib.test.js
@@ -122,3 +122,18 @@ describe("notifyCustomer", () => {
     expect(mailSent).toBe(true);
   });
 });
+
+// a better way to test the notifyCustomer function
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
+
+    lib.notifyCustomer({ customerId: 1 });
+
+    expect(mail.send).toHaveBeenCalled();
+    // expect(mail.send).toHaveBeenCalledWith("a", "Your order was placed successfully.");
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
+  });
+});
