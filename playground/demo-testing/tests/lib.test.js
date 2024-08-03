e947996a8d39564aaafed18ea288231b51f8a50e
IanD1013 <ian.tung1013@gmail.com>
2024-08-03 20:37:06 +1200

    Testing strings

--- playground/demo-testing/tests/lib.test.js
+++ playground/demo-testing/tests/lib.test.js
@@ -19,3 +19,12 @@ describe("absolute", () => {
     expect(result).toBe(0);
   });
 });
+
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("Mosh");
+
+    expect(result).toMatch(/Mosh/);
+    expect(result).toContain("Mosh");
+  });
+});
