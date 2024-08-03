831b8e7adab58cf9c251f600eca324e867105702
IanD1013 <ian.tung1013@gmail.com>
2024-08-03 23:43:42 +1200

    Set up the test Db

--- startup/config.js
+++ startup/config.js
@@ -5,8 +5,8 @@ module.exports = function () {
   // console.log(`NODE_ENV: ${process.env.NODE_ENV}`); // return undefined if not set
   // console.log(`app: ${app.get("env")}`);            // return development by default
 
-  winston.info("Application Name: " + config.get("name"));
-  winston.info("Mail Server: " + config.get("mail.host"));
+  // winston.info("Application Name: " + config.get("name"));
+  // winston.info("Mail Server: " + config.get("mail.host"));
   // console.log("Mail Password: " + config.get("mail.password"));
 
   if (!config.get("jwtPrivateKey")) {
