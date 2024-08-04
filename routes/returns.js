d1f8a569b9f3fc2e249b8ca206a73c4f21c99001
IanD1013 <ian.tung1013@gmail.com>
2024-08-04 20:49:19 +1200

    Refactor the domain logic

--- routes/returns.js
+++ routes/returns.js
@@ -1,11 +1,10 @@
 const Joi = require("joi");
-const validate = require("../middleware/validate");
 const express = require("express");
 const router = express.Router();
 const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
+const validate = require("../middleware/validate");
 const auth = require("../middleware/auth");
-const moment = require("moment");
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
@@ -15,9 +14,7 @@ router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   if (rental.dateReturned)
     return res.status(400).send("Return already processed");
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.updateOne(
@@ -27,7 +24,7 @@ router.post("/", [auth, validate(validateReturn)], async (req, res) => {
     }
   );
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 });
 
 function validateReturn(req) {
