9ec919cfae92545a0a979902cebc7019ed696a36
IanD1013 <ian.tung1013@gmail.com>
2024-08-04 14:12:34 +1200

    Testing the Authroization

--- startup/routes.js
+++ startup/routes.js
@@ -6,6 +6,7 @@ const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const returns = require("../routes/returns");
 const home = require("../routes/home");
 const error = require("../middleware/error");
 
@@ -22,6 +23,7 @@ module.exports = function (app) {
   app.use("/api/rentals", rentals);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/returns", returns);
   app.use("/", home);
   app.use(error);
 };
