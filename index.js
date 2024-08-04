2f210ae4ffdbf496d1b6cae5dd3b43ad949eb582
IanD1013 <ian.tung1013@gmail.com>
2024-08-04 20:56:33 +1200

    Prepare the app for production

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const winston = require("winston");
-const debug = require("debug")("app:startup");
-const morgan = require("morgan");
-const helmet = require("helmet");
-
 const express = require("express");
 app = express();
 
@@ -11,12 +7,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-
-app.use(helmet());
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  debug("Morgan enabled...");
-}
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
